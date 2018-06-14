9052cee1ed5fc03556cff6db443387fd34a07624
Aleksey1999 <akondr1999@gmail.com>
2018-06-14 21:46:08 +0300

    Initial commit

---
+++ app/src/main/java/com/example/aleksey/variance_analysis/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.aleksey.variance_analysis;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()) {
+                        case R.id.nav_a:
+                            selectedFragment = new OneFragment();
+                            break;
+                        case R.id.nav_ab:
+                            selectedFragment = new TwoFragment();
+                            break;
+                        case R.id.nav_abc:
+                            selectedFragment = new ThreeFragment();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+                    return true;
+                }
+            };
+
+
+
+}
